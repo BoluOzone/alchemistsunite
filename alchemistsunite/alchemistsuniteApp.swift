8ddafdd6615727a37394993fd44303edbfb4b143
Bolu <bolu.oladun@gmail.com>
2021-07-29 23:26:52 +0100

    Initial Commit

---
+++ alchemistsunite/alchemistsuniteApp.swift
@@ -0,0 +1,17 @@
+//
+//  alchemistsuniteApp.swift
+//  alchemistsunite
+//
+//  Created by Bolu Oladunjoye on 29.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct alchemistsuniteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
